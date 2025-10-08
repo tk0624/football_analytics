7fa936b5163115accab5d0e8b46a53599e414de0
tk0624 <tkakiuchi0624@icloud.com>
2025-10-08 22:01:08 +0900

    fix_251008-2201

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+// pages/_document.tsx
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="ja">
+      <Head>
+        {/* ✅ これがないとiPhone表示が崩れる */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
