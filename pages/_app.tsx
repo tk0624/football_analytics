ba31bdfe8dcb7a2e0b9ba21e42483450a3bb67e8
tk0624 <tkakiuchi0624@icloud.com>
2026-03-02 22:27:47 +0900

    Fix viewport meta tag warning: move from _document to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 // pages/_app.tsx
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { I18nProvider } from "../contexts/I18n";
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <I18nProvider>
-      {/* 全ページに重ね表示 */}
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Component {...pageProps} />
     </I18nProvider>
   );
