b8a80fe9e037339f00060671e25eef800f98b96e
tk0624 <tkakiuchi0624@icloud.com>
2026-03-05 17:11:32 +0900

    Feat: add Google Analytics (G-X65HTWJ83R)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,28 @@
 // pages/_document.tsx
 import { Html, Head, Main, NextScript } from "next/document";
 
+const GA_ID = "G-X65HTWJ83R";
+
 export default function Document() {
   return (
     <Html lang="ja">
-      <Head />
+      <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_ID}');
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
